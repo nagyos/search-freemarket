7d615c5d1bf1f96247b5758fd8e7bea08c05199d
shoki-tagawa <pupugshoki.wakaru.to.dekiru@gmail.com>
2022-12-05 09:24:22 +0900

    ✅ fetch時のレスポンスステータスエラーハンドリング処理の追加

---
+++ next-app/src/handler/error.ts
@@ -0,0 +1,20 @@
+export const handleErrors = (res: Response) => {
+  if (res.ok) {
+    return res
+  }
+
+  switch (res.status) {
+    case 400:
+      throw Error('INVALID_TOKEN')
+    case 401:
+      throw Error('UNAUTHORIZED')
+    case 500:
+      throw Error('INTERNAL_SERVER_ERROR')
+    case 502:
+      throw Error('BAD_GATEWAY')
+    case 404:
+      throw Error('NOT_FOUND')
+    default:
+      throw Error('UNHANDLED_ERROR')
+  }
+}
